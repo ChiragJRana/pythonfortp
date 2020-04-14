690a5f0707cfddd76e5c739e81d3a860a10c7a58
Chirag Rana <42936626+ChiragJRana@users.noreply.github.com>
2020-04-14 23:17:07 +0530

    Used the googletrans library

---
+++ googletranslate.py
@@ -0,0 +1,15 @@
+import googletrans
+
+def main(text):
+    translator = googletrans.Translator()
+    lang = input('Enter the language').lower()
+    if lang in googletrans.LANGCODES:
+        print(translator.translate(text=text,dest=googletrans.LANGCODES[lang]))
+    else:
+        print('Incorrect language')
+        print(*[x for x in googletrans.LANGCODES],sep='\n')
+        print('Try using any languages from these')
+
+if __name__ == '__main__':
+    text = input('Enter the text')
+    main(text)
