f07a7a74d875e25dcc3f7826c4bf4d0fb7f814ef
Chirag Rana <42936626+ChiragJRana@users.noreply.github.com>
2020-05-30 19:35:31 +0530

    Used Pillow as an image processing tool

--- googletranslate.py
+++ googletranslate.py
@@ -4,11 +4,12 @@ def main(text):
     translator = googletrans.Translator()
     lang = input('Enter the language').lower()
     if lang in googletrans.LANGCODES:
-        print(translator.translate(text=text,dest=googletrans.LANGCODES[lang]))
+        print(translator.translate(text=text,dest=googletrans.LANGCODES[lang]).text)
     else:
         print('Incorrect language')
         print(*[x for x in googletrans.LANGCODES],sep='\n')
         print('Try using any languages from these')
+
 
 if __name__ == '__main__':
     text = input('Enter the text')
